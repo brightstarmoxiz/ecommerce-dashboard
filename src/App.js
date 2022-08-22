00f102aa680a74ffac05ba2f9c91970cba140ed4
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-22 09:28:18 +0100

    new world

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './App.css';
 
 function App() {
-  return (
-  <h1 style={{textDecoration:"underline text-bold"}}>app of reat</h1>
+  return (                   
+  <h1 style={{textDecoration:"underline"}}>app of react</h1>
   );
 }
 
-export default App;
+export default App

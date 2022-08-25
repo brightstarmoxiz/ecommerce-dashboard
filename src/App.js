df50503e26ed415092e96946f0287cb06c39dc39
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-25 07:45:42 +0100

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, {useEffect} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
-import {TooltipComponent} from '@syncfusion/ej2-react'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
 import './App.css';
 
 function App() {
+  const activeMenu = false;
   return (                   
-    <h1 className="text-3xl font-bold underline">
-    hello brightstar!
-  </h1>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+      <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+        <TooltipComponent content="settings" position="Top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl
+            hover:bg-light-gray text-white" style={{background:'blue', borderRadius:'50%'}}>
+              <FiSettings/>
+            </button>
+        </TooltipComponent>
+      </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg 
+          bg-white">
+            Sidebar
+          </div>
+):(
+  <div className="w-0 dark:bg-secondary-dark-bg">
+     Sidebar w-0
+  </div>
+        )}
+        <div className={
+          
+        }>
+
+        </div>
+      </div>
+      </BrowserRouter>
+    </div>
   );
 }
 

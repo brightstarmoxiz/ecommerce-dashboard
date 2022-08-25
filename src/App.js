3a67a81750e3ca99e774d37295b4c0a099064bf0
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-25 09:34:07 +0100

    tailwind solution

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ function App() {
   </div>
         )}
         <div className={
-          
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? ' md:ml-72 ':
+           'flex-2'}`
+        
         }>
 
         </div>

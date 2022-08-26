2fb6e807b1c8a0e1a33d3a2f8ccb76c7e4e3c999
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-26 07:16:27 +0100

    new file

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div>
+            Botton
+        </div>
+    )
+}
+
+export default Button
---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ThemeSettings = () => {
+    return (
+        <div>
+            ThemeSettings
+ThemeSettings
+        </div>
+    )
+}
+
+export default ThemeSettings

c52d08e6c974e4073e07087d7c70e6c622e090b7
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-27 06:43:54 +0100

    renew

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import { SiShopware} from 'react-icons/si';
 import { MdOutlineCancel} from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 
 import {links} from '../data/brightstar';
@@ -13,7 +13,12 @@ const activeMenu=true;
         <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
            {activeMenu && (<>
         <div className="flex justify-between items-center">
-        <Link to="/" onclick={() =>{}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tacking-tight dark:text-white text-slate-900"><SiShopware/> <span> Shoppy</span></Link>
+        <Link to="/" onClick={() =>{}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tacking-tight dark:text-white text-slate-900"><SiShopware/> <span> Shoppy</span></Link>
+        <TooltipComponent content="Menu"  position="BottomCenter">
+                    <button>
+                        <MdOutlineCancel/>
+                    </button>
+        </TooltipComponent >
         </div>
            </> )}
 

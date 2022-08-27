fbc87303bc22d9c26d6bd887dad9abec599279b5
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-27 04:59:49 +0100

    new work

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import { SiShopware} from 'react-icons/si';
+import { MdOutlineCancel} from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+
+import {links} from '../data/brightstar';
 const Sidebar = () => {
+const activeMenu=true;
+
     return (
-        <div>
-            Sidebar
-Sidebar
+        <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+           {activeMenu && (<>
+        <div className="flex justify-between items-center">
+        <Link to="/" onclick={() =>{}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tacking-tight dark:text-white text-slate-900"><SiShopware/> <span> Shoppy</span></Link>
+        </div>
+           </> )}
+
         </div>
     )
 }

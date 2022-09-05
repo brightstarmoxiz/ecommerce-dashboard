d653cb14af13890e0967055aefef805bd719d8fb
brightstarmoxiz <jojololamoses@gmail.com>
2022-09-05 12:37:29 +0100

    reactjf framwork

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{ useEffect} from 'react'
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi'; 
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg';
+import {Cart, Chat, Notification, UserProfile} from '.'
+import { useStateContext } from '../contexts/ContextProvider';
 
+const NavBotton = ({title, customFunc, icon, color, dotColor }) =>(
+    <TooltipComponent content= {title} position="BottomCenter" >
+            <button></button>
+    </TooltipComponent>
+)
 const Navbar = () => {
+    const { activeMenu,setActiveMenu} = useStateContext();
     return (
-        <div>
-            Navbar
+        <div className="flex justify-between p-2 md:mx-6 relative">
+          
 
         </div>
     )

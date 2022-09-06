35e4dfa00064c83b12ce087a4314788a725fb7a0
brightstarmoxiz <jojololamoses@gmail.com>
2022-09-06 05:17:18 +0100

    Navbajs update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,55 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({title, customFunc, icon, color, dotColor }) =>(
     <TooltipComponent content= {title} position="BottomCenter" >
-            <button></button>
+            <button type="button" onClick={customFunc} style={{color}}
+            className="relative text-xl rounded-full p-3 hover:bg-light-grey">
+                <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+                {icon}
+                </span>
+            </button>
     </TooltipComponent>
 )
 const Navbar = () => {
-    const { activeMenu,setActiveMenu} = useStateContext();
+    const { activeMenu,setActiveMenu,handleClick} = useStateContext();
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
-          <NavButton title ="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} colo="blue" icon = {<AiOutlineMenu/>}/>
+          <NavButton title ="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon = {<AiOutlineMenu/>}/>
+        <div className="flex">
+        <NavButton
+         title ="Cart" customFunc={() => handleClick('cart')} color="blue" icon = {<FiShoppingCart/>}/>
+  <NavButton
+         title ="Chart" 
+         dotColor="#03C907"
+         customFunc={() => handleClick('cart')} 
+         color="blue"
+          icon = {<BsChatLeft/>}/>
 
+<NavButton
+         title ="Notifications" 
+         dotColor="#03C907"
+         customFunc={() => handleClick('notification')} 
+         color="blue"
+          icon = {<RiNotification3Line/>}
+          />
+          <TooltipComponent 
+           content="Profile" position="BottomCenter"
+          >
+              <div className=" flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+               onClick={() => handleClick('useProfile')}>
+    <img 
+    className="rounded-full w-8 h-8"
+     src={avatar}
+      alt="profile" />
+      <p>
+          <span className="text-gray-400 text-14">Hi,</span> {' '}
+          <span className="text-gray-400 font-bold ml-1 text-14">Grace</span>
+      </p>
+      <MdKeyboardArrowDown className="text-gray-400 text-14"/>
+              </div>
+
+          </TooltipComponent>
+</div>
+            
         </div>
     )
 }

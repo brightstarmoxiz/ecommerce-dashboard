2d49b66067c14801b1ab0b0010e0169f73ab0079
brightstarmoxiz <jojololamoses@gmail.com>
2022-09-08 08:21:28 +0100

    ecommerce is updating

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -71,19 +71,22 @@ const Ecommerce = () => {
                       <div>
                         <p>
                           <span className="text-3xl font-semibold">$90,492</span>
-                         
+                          <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">20%</span>
                         </p>
                         <p className="text-gray-500 mt-1">Budget</p>
                       </div>
                       <div clasName="mt-8">
                         <p>
                           <span className="text-3xl font-semibold">$22,492</span>
-                          <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">20%</span>
+                         
                         </p>
                         <p className="text-gray-500 mt-1">Expense</p>
                       </div>
                       <div className="mt-5">
-                        <SparkLine currentColor="blue" />
+                        <SparkLine currentColor="blue"
+                        id="line-sparkline"
+                        type="Line
+                        " />
                       </div>
                     </div>
                    </div>
